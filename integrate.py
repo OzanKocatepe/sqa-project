d0eaabfda93672b9bafc1a910196e05d63c221df
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-01-07 16:21:52 +1100

    Simplified the Bloch equation function slightly in order to get a working prototype. Currently working on just plotting the single time correlations. They're plotted but I haven't checked if they're right. sigma+ and sigma- seem to always have the exact same curve.

--- integrate.py
+++ integrate.py
@@ -1,31 +1,54 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 
-def RadiativeDampedBlochEquations(t: float, t_prime: float, c: np.ndarray[float], n) -> np.ndarray[float]:
+def RadiativeDampedBlochEquations(t: float, c: np.ndarray[float]) -> np.ndarray[float]:
     """A function representing the optical Bloch equations with radiative damping from
     the (Kocabas et. al., 2012) paper - https://link.aps.org/doi/10.1103/PhysRevA.85.023817
     
     Parameters:
         t (float):
-            The first time considered by the operators. Not considered in c_1.
-        t_prime (float):
             The second time considered by the operators.
             This is also the variable we are solving the equations with respect to.
         c (float):
             The vector of correlation functions which we are solving for.
-        n (float):
-            The degree of the correlation function we are solving for.
-            c_1, c_2, and c_3 respectively are defined in the above paper
-            in equation (5).
     """
 
+    # All time scales are given in units of tau, and all frequencies
+    # are given in units of 1 / tau.
+    tau = 1
+    atomicTransitionFrequency = 1
+    rabiFrequency = 1
+    photonFrequency = 1
+
+    # Defines useful terms for coefficient matrix.
+    omegaTerm = 1 + 1j * atomicTransitionFrequency
+    expTerm = rabiFrequency * np.exp(1j * photonFrequency * t, dtype=complex)
+
+
     # Coefficient matrix.
-    B = np.array([[0, 0, 0],
-                  [0, 0, 0],
-                  [0, 0, 0]])
+    B = (1 / tau) * np.array([[-omegaTerm  ,   0                        ,   0.5j * expTerm.conjugate()],
+                              [0           ,   -omegaTerm.conjugate()   ,   -0.5j * expTerm           ],
+                              [1j * expTerm,   -1j * expTerm.conjugate(),   -2                        ]], dtype=complex)
 
     # Inhomogenous term.
-    b = np.array([0, 0, 0])
+    b = np.array([0, 0, -2 / tau])
 
     # Returns the ODE.
     return B @ c + b
+
+initialConditions = np.array([-1, -1, -1]) # Atom is in ground state.
+timeLimit = 30 # Going up to 5 tau time intervals.
+xAxis = np.linspace(0, timeLimit, 100) # Points to evaluate the solution at.
+
+# Solves the ODE.
+solution = solve_ivp(fun=RadiativeDampedBlochEquations, t_span=(0, timeLimit), y0=initialConditions, t_eval=xAxis)
+
+# Plots the ODE.
+plt.plot(solution.t, solution.y[0], color='blue', label='$\langle \sigma_- (t) \\rangle$', linestyle='dashed')
+plt.plot(solution.t, solution.y[1], color='red', label='$\langle \sigma_+ (t) \\rangle$', linestyle='dashed')
+plt.plot(solution.t, solution.y[2], color='green', label='$\langle \sigma_z(t) \\rangle$', linestyle='dashed')
+plt.xlabel("t' / $ \\tau $ (dimensionless)")
+plt.ylabel("Expectation")
+plt.legend()
+plt.show()
