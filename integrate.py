Attempted to plot the analytic solutions, but they are completely wrong. The analytic solution seems more physically impossible than the numerical solution, so I think I took the inverse laplace wrong. At any rate, I'm going to try using the time-independent Bloch equations the paper suggested because they seem simpler and more aligned with what makes a numerical simulation simple and easy. Since the transformed operators have the same form as the laplaced functions, it should be easier to compare numerical and analytical derivations. Also need to clean up the code.

integrate.py
@@ -1,6 +1,14 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from mpmath import invertlaplace
+
+# All time scales are given in units of tau, and all frequencies
+# are given in units of 1 / tau.
+tau = 1
+atomicTransitionFrequency = 1
+rabiFrequency = 1
+photonFrequency = 1
 
 def RadiativeDampedBlochEquations(t: float, c: np.ndarray[float]) -> np.ndarray[float]:
     """A function representing the optical Bloch equations with radiative damping from
@@ -13,13 +21,6 @@ def RadiativeDampedBlochEquations(t: float, c: np.ndarray[float]) -> np.ndarray[
         c (float):
             The vector of correlation functions which we are solving for.
     """
-
-    # All time scales are given in units of tau, and all frequencies
-    # are given in units of 1 / tau.
-    tau = 1
-    atomicTransitionFrequency = 1
-    rabiFrequency = 1
-    photonFrequency = 1
 
     # Defines useful terms for coefficient matrix.
     omegaTerm = 1 + 1j * atomicTransitionFrequency
@@ -37,17 +38,25 @@ def RadiativeDampedBlochEquations(t: float, c: np.ndarray[float]) -> np.ndarray[
     # Returns the ODE.
     return B @ c + b
 
-initialConditions = np.array([-1, -1, -1]) # Atom is in ground state.
+initialConditions = np.array([-1, -1, -1], dtype=complex) # Atom is in ground state.
 timeLimit = 30 # Going up to 5 tau time intervals.
 xAxis = np.linspace(0, timeLimit, 100) # Points to evaluate the solution at.
 
 # Solves the ODE.
 solution = solve_ivp(fun=RadiativeDampedBlochEquations, t_span=(0, timeLimit), y0=initialConditions, t_eval=xAxis)
 
+def P(s: float) -> float:
+    return (s + 2 / tau) * ( (s + 1 / tau)**2 + (atomicTransitionFrequency - photonFrequency)**2 ) + rabiFrequency**2 * (s + 1 / tau)
+
+# Manually writes out the Laplace transformed ODEs.
+laplaceMinusExpectation = lambda s: -0.5j * rabiFrequency * (s + 2 / tau) * (s + 1 / tau - 1j * (atomicTransitionFrequency - photonFrequency)) / (s * P(s))
+analyticMinusExpectation = np.array([invertlaplace(laplaceMinusExpectation, t) for t in xAxis[1:]])
+
 # Plots the ODE.
-plt.plot(solution.t, solution.y[0], color='blue', label='$\langle \sigma_- (t) \\rangle$', linestyle='dashed')
-plt.plot(solution.t, solution.y[1], color='red', label='$\langle \sigma_+ (t) \\rangle$', linestyle='dashed')
-plt.plot(solution.t, solution.y[2], color='green', label='$\langle \sigma_z(t) \\rangle$', linestyle='dashed')
+plt.plot(solution.t, solution.y[0], color='blue', label='$\\langle \\sigma_- (t) \\rangle$', linestyle='dashed')
+plt.plot(xAxis[1:], analyticMinusExpectation, color='red', label='Analytic $\\langle \\sigma_- (t) \\rangle$', linestyle='dashed')
+# plt.plot(solution.t, solution.y[1], color='red', label='$\\langle \\sigma_+ (t) \\rangle$', linestyle='dashed')
+# plt.plot(solution.t, solution.y[2], color='green', label='$\\langle \\sigma_z(t) \\rangle$', linestyle='dashed')
 plt.xlabel("t' / $ \\tau $ (dimensionless)")
 plt.ylabel("Expectation")
 plt.legend()