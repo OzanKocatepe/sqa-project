927e84fda6b2fc179980de971d546f31bcb3aa0d
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-01-08 18:17:53 +1100

    Rewrote the code so that it finds the numerical and analytical solutions in both the time and laplace domains. The time domains don't match up at all; the laplace domains match up much better, though at the end of my range (for any arbitrary range) the laplace form breaks, so there must be something in the way I'm calculating it that makes it break at the edge. The parts that do agree agree up to 1e-3, but that feels larger then floating point inaccuracy (thought it may be within uncertainty since we're using numerical methods).

--- integrate2.py
+++ integrate2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from mpmath import invertlaplace
 
 tau = 1        # Characteristic decay scale.
@@ -43,39 +44,60 @@ def TimeIndependentBlochEquations(t: np.typing.ArrayLike, c: np.ndarray[float]) 
 
     return M @ c + b
 
+# ====================================
+# ==== FINDING NUMERICAL SOLUTION ====
+# ====================================
+
 # Atom is in ground state at t = 0.
 initialConditions = np.array([0, 0, -1], dtype=complex)
 
-# Plotting up to this many tau time intervals.
-timeLimit = 30
-
 # The points within the range (0, timeLimit) we will evaluate the numerical solution at.
-tAxis = np.linspace(0, timeLimit, 100)
-# The points that we evaluate the Laplaced solutions at.
-# sAxis = np.linspace(0.001, 0.2, 100)
+tDomain = (0, 300)
+tAxis = np.linspace(0, tDomain[1], 1000)
+# The points (in the complex plane) that we evaluate the Laplaced solutions at.
+sRange = 0.2
+sRealAxis, sImagAxis = np.meshgrid(np.linspace(-sRange, sRange, 20), np.linspace(-sRange, sRange, 20))
+sAxis = sRealAxis + 1j * sImagAxis
 
 # Numerically solves the ODE for n = 1.
-numericalSol = integrate.solve_ivp(fun=TimeIndependentBlochEquations, t_span=(0, timeLimit), y0=initialConditions, t_eval=tAxis)
+numericalSamples = integrate.solve_ivp(fun=TimeIndependentBlochEquations, t_span=tDomain, y0=initialConditions, t_eval=tAxis)
 
-# Defines the analytical solution for the time-independent sigma-minus expectation
-# in the laplace domain as a function of s.
+# =====================================
+# ==== FINDING ANALYTICAL SOLUTION ====
+# =====================================
+
+# Defines the analytical solution for the Laplace of tilde sigma-minus as a function of s.
 P = lambda s: (s + 2 / tau) * ( (s + 1 / tau)**2 + omegaTilde**2 ) + rabiFreq**2 * (s + 1 / tau)
-analyticalFunc = lambda s: -0.5j * rabiFreq * (s + 2 / tau) * (s + 1 / tau - 1j * omegaTilde) / (s * P(s))
-# Takes the inverse laplace of the solution and evaluates it at each time t that we evaluated the numerical solution at.
-analyticalSol = np.array([invertlaplace(analyticalFunc, t) for t in numericalSol.t[1:]])
+analyticalLaplaceSol = lambda s: -0.5j * rabiFreq * (s + 2 / tau) * (s + 1 / tau - 1j * omegaTilde) / (s * P(s))
+# Evaluates the analytical solution in the complex plane.
+analyticalLaplaceSamples = analyticalLaplaceSol(sAxis)
 
-# Evaluates the analytical solution at the points that we desire.
-# analyticalSol = analyticalFunc(sAxis)
-# Takes the Laplace Transform of the numerical solution.
-# numericalLaplacedFunc = lambda s: integrate.simpson(numericalSol.y[0] * np.exp(-s * numericalSol.t), numericalSol.t)
-# numericalLaplacedSol = np.array([numericalLaplacedFunc(s) for s in sAxis])
+# =================================================
+# ==== LAPLACE TRANSFORM OF NUMERICAL SOLUTION ====
+# =================================================
 
-# diff = numericalSol.y[0] - analyticalSol
-# print(np.vstack((numericalSol.y[0], analyticalSol, diff)).T)
+# Defines the laplace transformation of the function as a function of s.
+numericalLaplaceSol = lambda s: integrate.simpson(
+    numericalSamples.y[0] * np.exp(-s * numericalSamples.t), # Laplace transform integrand.
+    numericalSamples.t                                       # Points at which numerical solution was evaluated.
+)
+# Evaluates the laplaced numerical solution at the desired points in the complex plane.
+numericalLaplaceSamples = np.zeros(shape=(sAxis.shape), dtype=complex)
+for x in np.arange(sAxis.shape[0]):
+    for y in np.arange(sAxis.shape[1]):
+        numericalLaplaceSamples[x, y] = numericalLaplaceSol(sAxis[x, y])
 
-# ===========================================================
-# Plotting analytical and numerical solutions for sigma-minus.
-# ===========================================================
+# ====================================================
+# ==== INVERSE LAPLACE OF THE ANALYTICAL SOLUTION ====
+# ====================================================
+
+# Evaluates the inverse laplace transform of the equation in the papers for each time that we desire.
+# Doesn't calculate at t = 0, because that ends up with a division by 0 apparently.
+analyticalSamples = np.array([invertlaplace(analyticalLaplaceSol, t) for t in tAxis[1:]])
+
+# ========================================
+# ==== PLOTTING TIME DOMAIN SOLUTIONS ====
+# ========================================
 
 numColor = 'black'
 analyticColor = 'blue'
@@ -87,26 +109,96 @@ imagStyle = 'dotted'
 fig, ax = plt.subplots(1, 3, figsize=(32, 6))
 
 # Magnitude of expectation.
-ax[0].plot(tAxis, np.abs(numericalSol.y[0]), color=numColor, label='Numerical Magnitude', linestyle=magStyle)
-ax[0].plot(tAxis[1:], np.abs(analyticalSol), color=analyticColor, label='Analytical Magnitude', linestyle=magStyle)
+ax[0].plot(tAxis, np.abs(numericalSamples.y[0]), color=numColor, label='Numerical Magnitude', linestyle=magStyle)
+ax[0].plot(tAxis[1:], np.abs(analyticalSamples), color=analyticColor, label='Analytical Magnitude', linestyle=magStyle)
 
 # Real part of expectation.
-ax[1].plot(tAxis, numericalSol.y[0].real, color=numColor, label='Numerical Real Part', linestyle=realStyle)
-ax[1].plot(tAxis[1:], analyticalSol.real, color=analyticColor, label='Analytical Real Part', linestyle=realStyle)
+ax[1].plot(tAxis, numericalSamples.y[0].real, color=numColor, label='Numerical Real Part', linestyle=realStyle)
+ax[1].plot(tAxis[1:], analyticalSamples.real, color=analyticColor, label='Analytical Real Part', linestyle=realStyle)
 
 # Imaginary part of expectation.
-ax[2].plot(tAxis, numericalSol.y[0].imag, color=numColor, label='Numerical Imaginary Part', linestyle=imagStyle)
-ax[2].plot(tAxis[1:], analyticalSol.imag, color=analyticColor, label='Analytical Imaginary Part', linestyle=imagStyle)
+ax[2].plot(tAxis, numericalSamples.y[0].imag, color=numColor, label='Numerical Imaginary Part', linestyle=imagStyle)
+ax[2].plot(tAxis[1:], analyticalSamples.imag, color=analyticColor, label='Analytical Imaginary Part', linestyle=imagStyle)
 
 xLabel = "$t / \\tau$"
 ax[0].set_xlabel(xLabel)
 ax[1].set_xlabel(xLabel)
 ax[2].set_xlabel(xLabel)
 
-ax[0].set_ylabel("$\\langle \\tilde{\\sigma}_-(t) \\rangle$")
+ax[0].set_ylabel("$\\left\\| \\langle \\tilde{\\sigma}_-(t) \\rangle \\right\\|$")
+ax[1].set_ylabel("\\text{Re} \\left[ \\langle \\tilde{\\sigma}_-(t) \\rangle \\right]")
+ax[2].set_ylabel("\\text{Im} \\left[ \\langle \\tilde{\\sigma}_-(t) \\rangle \\right]")
 
 ax[0].legend()
 ax[1].legend()
 ax[2].legend()
+
+plt.show()
+
+# ===========================================
+# ==== PLOTTING LAPLACE DOMAIN SOLUTIONS ====
+# ===========================================
+
+fig, ax = plt.subplots(1, 3, figsize=(32, 6), subplot_kw={'projection' : '3d'})
+
+ax[0].plot_surface(sAxis.real, sAxis.imag, np.abs(numericalLaplaceSamples), color=numColor, label="Numerical Magnitude")
+ax[0].plot_surface(sAxis.real, sAxis.imag, np.abs(analyticalLaplaceSamples), color=analyticColor, label="Analytical Magnitude")
+
+ax[1].plot_surface(sAxis.real, sAxis.imag, numericalLaplaceSamples.real, color=numColor, label="Numerical Magnitude")
+ax[1].plot_surface(sAxis.real, sAxis.imag, analyticalLaplaceSamples.real, color=analyticColor, label="Analytical Magnitude")
+
+ax[2].plot_surface(sAxis.real, sAxis.imag, numericalLaplaceSamples.imag, color=numColor, label="Numerical Magnitude")
+ax[2].plot_surface(sAxis.real, sAxis.imag, analyticalLaplaceSamples.imag, color=analyticColor, label="Analytical Magnitude")
+
+xLabel = "$\\text{Re}(s)$"
+ax[0].set_xlabel(xLabel)
+ax[1].set_xlabel(xLabel)
+ax[2].set_xlabel(xLabel)
+
+yLabel = "$\\text{Im}(s)$"
+ax[0].set_ylabel(yLabel)
+ax[1].set_ylabel(yLabel)
+ax[2].set_ylabel(yLabel)
+
+ax[0].set_zlabel("$\\left\\| \\mathcal{L}_t \\langle \\tilde{\\sigma}_-(t) \\rangle \\right\\|$")
+ax[1].set_zlabel("$\\text{Re} \\left[ \\mathcal{L}_t \\langle \\tilde{\\sigma}_-(t) \\rangle \\right]$")
+ax[2].set_zlabel("$\\text{Im} \\left[ \\mathcal{L}_t \\langle \\tilde{\\sigma}_-(t) \\rangle \\right]$")
+
+ax[0].legend()
+ax[1].legend()
+ax[2].legend()
+
+plt.show()
+
+# ============================================
+# ==== PLOTTING LAPLACE DOMAIN DIFFERENCE ====
+# ============================================
+
+zeroColor = 'red'
+fig, ax = plt.subplots(1, 3, figsize=(32, 6), subplot_kw={'projection' : '3d'})
+key = np.abs(numericalLaplaceSamples - analyticalLaplaceSamples) <= 1e-3
+
+# Maps the key values to colors.
+colorMap = np.zeros((sAxis.shape[0], sAxis.shape[1], 3), dtype=float)
+colorMap[key] = mcolors.to_rgb(zeroColor)
+colorMap[~key] = mcolors.to_rgb(numColor)
+
+ax[0].plot_surface(sAxis.real, sAxis.imag, np.abs(numericalLaplaceSamples - analyticalLaplaceSamples), facecolors=colorMap, shade=False)
+ax[1].plot_surface(sAxis.real, sAxis.imag, (numericalLaplaceSamples - analyticalLaplaceSamples).real, facecolors=colorMap, shade=False)
+ax[2].plot_surface(sAxis.real, sAxis.imag, (numericalLaplaceSamples - analyticalLaplaceSamples).imag, facecolors=colorMap, shade=False)
+
+xLabel = "$\\text{Re}(s)$"
+ax[0].set_xlabel(xLabel)
+ax[1].set_xlabel(xLabel)
+ax[2].set_xlabel(xLabel)
+
+yLabel = "$\\text{Im}(s)$"
+ax[0].set_ylabel(yLabel)
+ax[1].set_ylabel(yLabel)
+ax[2].set_ylabel(yLabel)
+
+ax[0].set_zlabel("Norm of Difference")
+ax[1].set_zlabel("Real Part of Difference")
+ax[2].set_zlabel("Imaginary Part of Difference")
 
 plt.show()
