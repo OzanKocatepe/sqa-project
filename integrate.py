da2119df3495eb266f0ca463005f92a520dd747b
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-01-13 11:29:21 +1100

    I added functionality to also plot the double-time correlation functions with their separate inhomogenous terms and initial conditions. Both of these have closed forms since we are considering t -> inf (steady-state) and so it was quite easy, each loop can be completed independently. Furthermore, I changed some of the labels on the plots to be more consistent in labelling t, t', and delta t.

--- integrate.py
+++ integrate.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import sympy
 
 tau = 1             # Characteristic decay scale.
-detuningFreq = 0    # Normalised detuning frequency, described in Appendix B.
-rabiFreq = 2        # Normalised rabi frequency, described in appendix B.
+detuningFreq = 0    # Normalised detuning frequency, described in Appendix B. Denoted D in the paper.
+rabiFreq = 2        # Normalised rabi frequency, described in appendix B. Denoted R in the paper.
 
 def TimeIndependentBlochEquations(t: np.typing.ArrayLike, c: np.ndarray[float], b: float) -> np.ndarray[float]:
     """
@@ -49,104 +49,152 @@ def TimeIndependentBlochEquations(t: np.typing.ArrayLike, c: np.ndarray[float], 
 # ==== FINDING NUMERICAL SOLUTION ====
 # ====================================
 
-# Atom is in ground state at t = 0.
-initialConditions = np.array([0, 0, -1], dtype=complex)
+"""
+Loop through the single and double time correlations.
+The most important distinctions between these are:
+    - The single time correlation is a function of t', while the double time correlations are functions of t and t'.
+      However, we choose t -> inf and t' = t + dt, so in a sense both are just a function of dt.
+    - Single and double time correlations have different initial conditions
+      and inhomogenous terms.
+"""
+for n in np.arange(2):
+    if n == 0:
+        # Atom is in ground state at t = 0.
+        initialConditions = np.array([0, 0, -1], dtype=complex)
 
-# The points within the range (0, timeLimit) we will evaluate the numerical solution at.
-tDomain = (0, 50)
-tAxis = np.linspace(tDomain[0], tDomain[1], 1000)
-
-# Numerically solves the ODE for n = 1.
-numericalSol = integrate.solve_ivp(fun=TimeIndependentBlochEquations,
-                                   t_span=tDomain,
-                                   y0=initialConditions,
-                                   t_eval=tAxis,
-                                   rtol=1e-10,
-                                   atol=1e-12,
-                                   args=(-2 / tau,))
-
-# =========================================
-# ==== FINDING THE ANALYTICAL SOLUTION ====
-# =========================================
-
-# Defines the analytical solutions symbolically using sympy.
-s, tSym, tauSym, detuningFreqSym, rabiFreqSym = sympy.symbols('s, t, tau, detuningFreq, rabiFreq')
-
-# P is the function P(s) defined in equation (B4)
-P = (s + 2 / tauSym) * ( (s + 1 / tauSym)**2 + detuningFreqSym**2) + rabiFreqSym**2 * (s + 1 / tauSym)
-# This is a vector containing the equations (B1-B3)
-analyticalLaplaceSym = sympy.Matrix([-0.5j * rabiFreqSym * (s + 2 / tauSym) * (s + 1 / tauSym - 1j * detuningFreqSym) / (s * P),
-                                     0.5j * rabiFreqSym * (s + 2 / tauSym) * (s + 1 / tauSym + 1j * detuningFreqSym) / (s * P),
-                                     -(s + 2 / tauSym) * ( (s + 1 / tauSym)**2 + detuningFreq**2) / (s * P)])
-
-# Simplifies the expression.
-analyticalLaplaceSym = sympy.simplify(analyticalLaplaceSym)
-
-# Substitutes the numerical values of our parameters.
-analyticalLaplaceSym = analyticalLaplaceSym.subs([
-    (tauSym, tau),
-    (detuningFreqSym, detuningFreq),
-    (rabiFreqSym, rabiFreq)
-])
-
-# Performs partial fraction decomposition.
-analyticalLaplaceSym = sympy.apart(analyticalLaplaceSym, s)
-
-# Simplifies with the new numerical values.
-analyticalLaplaceSym = sympy.simplify(analyticalLaplaceSym)
-
-# Takes the inverse laplace transform symbolically.
-analyticalSolSym = sympy.inverse_laplace_transform(analyticalLaplaceSym, s, tSym)
-
-# Converts it to a numerical function.
-analyticalFunc = sympy.lambdify(tSym, analyticalSolSym, modules=['numpy'])
-# Calculates the desired time domain values.
-analyticalSamples = analyticalFunc(tAxis)
-
-# ========================================
-# ==== PLOTTING TIME DOMAIN SOLUTIONS ====
-# ========================================
-
-numericalColor = 'black'
-analyticalColor = 'blue'
-
-figSize = (16, 8.8)
-tLimit = 10
-plottingFunctions = [np.abs, lambda z: z.real, lambda z: z.imag] # The function each column should plot.
-tLineStyles = ['solid', 'dashed', 'dotted'] # The line style for each function/column.
-
-xLabel = r"$t / \tau$" # x-axis label.
-yLabels = [[r"$\left\| \langle \tilde{\sigma}_-(t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_-(t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_-(t) \rangle \right]$"],
-           [r"$\left\| \langle \tilde{\sigma}_+(t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_+(t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_+(t) \rangle \right]$"],
-           [r"$\left\| \langle \tilde{\sigma}_z(t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_z(t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_z(t) \rangle \right]$"]]
-
-# Creates the figure.
-nrows = 3
-ncols = len(plottingFunctions)
-fig, ax = plt.subplots(nrows, ncols, figsize=figSize)
-
-# Looping through the subplots.
-for row in np.arange(nrows):
-    for col in np.arange(ncols):
-        # Plot numerical solution.
-        ax[row, col].plot(tAxis, plottingFunctions[col](numericalSol.y[row]),
-                          color = numericalColor,
-                          label = "Numerical Sol",
-                          linestyle = tLineStyles[col])
+        # The inhomogenous term is just a constant.
+        inhomTerm = -2 / tau
+    elif n == 1:
+        # We take the initial condition as t' = t, with t -> inf, and so
+        # we can find the initial conditions using the steady-state solutions
+        # for the single time correlations, with some algebra.
+        initialConditions = np.array([0.25 * rabiFreq**2 / (1 + detuningFreq**2 + 0.5 * rabiFreq**2),
+                                      0,
+                                      -0.5j * rabiFreq * (1 + 1j * detuningFreq) / (1 + detuningFreq**2 + 0.5 * rabiFreq**2)], dtype=complex)
         
-        # Plots analytical solution.
-        ax[row, col].plot(tAxis, plottingFunctions[col](analyticalSamples[row, 0, :]),
-                          color = analyticalColor,
-                          label = "Analytical Sol",
-                          linestyle = tLineStyles[col])
+        # The inhomogenous term is sigma_+ at time t. Since for our initial conditions we are considering t -> infinity, we are
+        # always considering a t large enough that we can use the steady-state form of sigma_+, so we have a closed form for
+        # the inhomogenous term.
+        inhomTerm = -1 / tau * 1j * rabiFreq * (1 + 1j * detuningFreq) / (1 + detuningFreq**2 + 0.5 * rabiFreq**2)
         
-        # Sets other properties.
-        ax[row, col].set_xlim(0, tLimit)
-        ax[row, col].set_xlabel(xLabel)
-        ax[row, col].set_ylabel(yLabels[row][col])
-        ax[row, col].legend()
+    """
+    For the single time correlations (n = 1), we treat t = 0 and plot t' = 0 + dt, so we
+    simply label the time axis t. For the double time correlations (n = 2), we treat the initial time
+    as some t and take the steady-state solution as t -> inf, and then plot as a function of dt (i.e. how
+    separated the two operators are in time). For both scenarios, we are plotting the correlations
+    as a function of the time separation, dt).
+    """
 
-plt.suptitle(fr"$\tau = {tau}$, $D = {detuningFreq}$, $R = {rabiFreq}$")
-plt.tight_layout()
-plt.savefig("Single-Time Correlation Functions", dpi=300)
-plt.show()
+    # The points within the range (0, timeLimit) we will evaluate the numerical solution at.
+    tDomain = (1e-6, 10)
+    tAxis = np.linspace(tDomain[0], tDomain[1], 500)
+
+    # Numerically solves the ODE.
+    numericalSol = integrate.solve_ivp(fun=TimeIndependentBlochEquations,
+                                       t_span=tDomain,
+                                       y0=initialConditions,
+                                       t_eval=tAxis,
+                                       rtol=1e-10,
+                                       atol=1e-12,
+                                       args=(inhomTerm,))
+
+    # =========================================
+    # ==== FINDING THE ANALYTICAL SOLUTION ====
+    # =========================================
+
+    # Defines the analytical solutions symbolically using sympy.
+    s, tSym, tauSym, detuningFreqSym, rabiFreqSym = sympy.symbols('s, t, tau, detuningFreq, rabiFreq')
+
+    # P is the function P(s) defined in equation (B4).
+    P = (s + 2 / tauSym) * ( (s + 1 / tauSym)**2 + detuningFreqSym**2) + rabiFreqSym**2 * (s + 1 / tauSym)
+
+    if n == 0:
+        # This is a vector containing the equations (B1-B3), which are the analytical solutions for the
+        # single-time correlation functions.
+        analyticalLaplaceSym = sympy.Matrix([-0.5j * rabiFreqSym * (s + 2 / tauSym) * (s + 1 / tauSym - 1j * detuningFreqSym) / (s * P),
+                                            0.5j * rabiFreqSym * (s + 2 / tauSym) * (s + 1 / tauSym + 1j * detuningFreqSym) / (s * P),
+                                            -(s + 2 / tauSym) * ( (s + 1 / tauSym)**2 + detuningFreq**2) / (s * P)])
+    elif n == 1:
+        # This is a 'vector' containing just the analytical solution for the first component of the
+        # double-time correlation functions. We have chosen at this time not to plot the remaining analytical
+        # solutions.
+        analyticalLaplaceSym = sympy.Matrix([ 0.25 * rabiFreqSym**2 * (P - 0.5 * rabiFreqSym**2 * (s + 2 / tauSym) / tauSym**2) / (s * P * (1 + detuningFreq**2 + 0.5 * rabiFreq**2)) ])
+
+    # Simplifies the expression.
+    analyticalLaplaceSym = sympy.simplify(analyticalLaplaceSym)
+
+    # Substitutes the numerical values of our parameters.
+    analyticalLaplaceSym = analyticalLaplaceSym.subs([
+        (tauSym, tau),
+        (detuningFreqSym, detuningFreq),
+        (rabiFreqSym, rabiFreq)
+    ])
+
+    # Performs partial fraction decomposition.
+    analyticalLaplaceSym = sympy.apart(analyticalLaplaceSym, s)
+
+    # Simplifies with the new numerical values.
+    analyticalLaplaceSym = sympy.simplify(analyticalLaplaceSym)
+
+    # Takes the inverse laplace transform symbolically.
+    analyticalSolSym = sympy.inverse_laplace_transform(analyticalLaplaceSym, s, tSym)
+
+    # Converts it to a numerical function.
+    analyticalFunc = sympy.lambdify(tSym, analyticalSolSym, modules=['numpy'])
+    # Calculates the desired time domain values.
+    analyticalSamples = analyticalFunc(tAxis)
+
+    # ========================================
+    # ==== PLOTTING TIME DOMAIN SOLUTIONS ====
+    # ========================================
+
+    numericalColor = 'black'
+    analyticalColor = 'blue'
+
+    figSize = (16, 8.8)
+    plottingFunctions = [np.abs, lambda z: z.real, lambda z: z.imag] # The function each column should plot.
+    tLineStyles = ['solid', 'dashed', 'dotted'] # The line style for each function/column.
+    titles = ["Single-Time Correlation Functions", "Double-Time Correlation Functions"]
+    xLabel = r"$\delta t / \tau$" # x-axis label.
+
+    if n == 0:
+        yLabels = [[r"$\left\| \langle \tilde{\sigma}_-(\delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_-(\delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_-(\delta t) \rangle \right]$"],
+                   [r"$\left\| \langle \tilde{\sigma}_+(\delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_+(\delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_+(\delta t) \rangle \right]$"],
+                   [r"$\left\| \langle \tilde{\sigma}_z(\delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_z(\delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_z(\delta t) \rangle \right]$"]]
+    elif n == 1:
+        yLabels = [[r"$\left\| \langle \tilde{\sigma}_(t) \tilde{\sigma}_-(t + \delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_-(t + \delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_-(t + \delta t) \rangle \right]$"],
+                   [r"$\left\| \langle \tilde{\sigma}_(t) \tilde{\sigma}_+(t + \delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_+(t + \delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_+(t + \delta t) \rangle \right]$"],
+                   [r"$\left\| \langle \tilde{\sigma}_(t) \tilde{\sigma}_z(t + \delta t) \rangle \right\|$", r"$\text{Re} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_z(t + \delta t) \rangle \right]$", r"$\text{Im} \left[ \langle \tilde{\sigma}_(t) \tilde{\sigma}_z(t + \delta t) \rangle \right]$"]]
+
+    # Creates the figure.
+    nrows = 3
+    ncols = len(plottingFunctions)
+    fig, ax = plt.subplots(nrows, ncols, figsize=figSize)
+
+    # Looping through the subplots.
+    for row in np.arange(nrows):
+        for col in np.arange(ncols):
+            # Plot numerical solution.
+            ax[row, col].plot(tAxis, plottingFunctions[col](numericalSol.y[row]),
+                            color = numericalColor,
+                            label = "Numerical Sol",
+                            linestyle = tLineStyles[col])
+        
+            # Plots analytical solution, as long as it is not the second or third components of
+            # the double-time correlation vector, since we have chosen not to compare those
+            # to analytical solutions.
+            if not (n == 1 and (row == 1 or row == 2)):
+                ax[row, col].plot(tAxis, plottingFunctions[col](analyticalSamples[row, 0, :]),
+                                color = analyticalColor,
+                                label = "Analytical Sol",
+                                linestyle = tLineStyles[col])
+        
+            # Sets other properties.
+            ax[row, col].set_xlabel(xLabel)
+            ax[row, col].set_ylabel(yLabels[row][col])
+            ax[row, col].legend()
+
+    plt.suptitle(fr"$\tau = {tau}$, $D = {detuningFreq}$, $R = {rabiFreq}$")
+    plt.tight_layout()
+    plt.savefig(titles[n], dpi=300)
+    plt.show()
