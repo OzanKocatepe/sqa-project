c090aa5d70ee42c649bcc41ec5544799a2c19446
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-01-25 19:41:33 +1100

    Moved all of the logic to the plotting class, and made it easier to plot specific momentum values, or the current operator for multiple k-values (now the title actually lists which momentums were plotted).

--- 1d-ssh-model.py
+++ 1d-ssh-model.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from SSHSimulation import SSHSimulation
+from SSHVisualiser import SSHVisualiser
 
 tAxis = np.linspace(0, 30, 10000)
 initialConditions = np.array([-0.5, -0.5, 0], dtype=complex)
 
-simulation = SSHSimulation( 
+sim = SSHSimulation( 
     t1 = 2,
     t2 = 1,
     decayConstant = 0.1,
@@ -14,42 +15,10 @@ simulation = SSHSimulation(
     drivingFreq = 2 / 3.01
 )
 
-simulation.AddMomentum([np.pi / 4, -np.pi / 4])
-simulation.Run(tAxis, initialConditions, steadyStateCutoff=15)
-currentOperator, currentOperatorFourier = simulation.CalculateTotalCurrent()
+sim.AddMomentum([np.pi / 4, -np.pi / 4])
+sim.Run(tAxis, initialConditions, steadyStateCutoff=15)
 
-# ==============================================
-# ==== PLOTTING EIGENBASIS CURRENT OPERATOR ====
-# ==============================================
-
-currentLabel = r"$\langle\tilde j_k \rangle$"
-yLabels = [
-    f"Magnitude of {currentLabel}",
-    f"Real Part of {currentLabel}",
-    f"Imaginary Part of {currentLabel}"
-]
-
-nrows, ncols = 3, 1
-fig, ax = plt.subplots(nrows, ncols, figsize=(16, 8.8))
-
-for row in np.arange(nrows):
-    # Plot the numerical solution.
-    ax[row].plot(tAxis, plottingFunctions[row](currentOperator),
-                color = "Black")
-    
-    ax[row].set_xlabel(xLabel)
-    ax[row].set_ylabel(yLabels[row])
-
-# plt.suptitle(title)
-plt.tight_layout()
-plt.show()
-
-# Plotting the fourier transform of the current operator.
-plt.semilogy(simulation.freqAxis / simulation.drivingFreq, np.abs(currentOperatorFourier)**2,
-        color = 'black')
-
-# plt.suptitle(title)
-# plt.xlim(-2.5, 2.5)
-plt.xlabel(r"$\omega / \Omega$")
-plt.ylabel(r"$\| \tilde j (\omega) \|^2$")
-plt.show()
+vis = SSHVisualiser(sim)
+vis.PlotSingleTimeCorrelations(np.pi / 4)
+vis.PlotSingleTimeCorrelations(-np.pi / 4)
+vis.PlotTotalCurrent()
--- SSHVisualiser.py
+++ SSHVisualiser.py
@@ -12,11 +12,13 @@ class SSHVisualiser:
         ----------
         sim : SSHSimulation
             The instance of SSHSimulation which stores all of the information that we
-            may want to plot. Should have already had Run() called.
+            may want to plot. Should have already had Run() called, since that relies on
+            other user inputs.
         """
 
         self._sim = sim
         self._plottingFunctions = [lambda z: np.abs(z), lambda z: z.real, lambda z: z.imag]
+        self._tLabel = r"$t \gamma_-$"
 
     def PlotSingleTimeCorrelations(self, k: float):
         r"""Plots the single-time correlations $\langle \tilde \sigma_-(t) \rangle,\, \langle \tilde \sigma_+(t) \rangle,\, \langle \tilde \sigma_z(t) \rangle$ for a fixed momentum.
@@ -27,12 +29,12 @@ class SSHVisualiser:
             The momentum for which we want to plot the correlation functions.
         """
 
+        model = self._sim._models[k]
+
         expectationLabels = [r"$\langle \tilde \sigma_-(t) \rangle$",
                              r"$\langle \tilde \sigma_+(t) \rangle$",
                              r"$\langle \tilde \sigma_z(t) \rangle$"]
                 
-        tLabel = r"$t \gamma_-$"
-
         yLabels = []
         for i in range(len(expectationLabels)):
             yLabels.append(
@@ -48,15 +50,55 @@ class SSHVisualiser:
         for row in np.arange(nrows):
             for col in np.arange(ncols):
                 # Plot numerical solution.
-                model = self._sim._models[k]
                 ax[row, col].plot(self._sim.tAxis, self._plottingFunctions[col](model.solution.y[row]),
                                 color = "Black")
         
                 # Sets other properties.
-                ax[row, col].set_xlabel(tLabel)
+                ax[row, col].set_xlabel(self._tLabel)
                 ax[row, col].set_ylabel(yLabels[row][col])
 
-        title = rf"$k = {self._sim.k / np.pi} \pi,\, t_1 = {self._sim.t1},\, t_2 = {self._sim.t2},\, A_0 = {self._sim.drivingAmplitude},\, \Omega = {self._sim.drivingFreq:.5f},\, \gamma_- = {self._sim.decayConstant}$"
+        title = rf"$k = {k / np.pi} \pi,\, t_1 = {self._sim.t1},\, t_2 = {self._sim.t2},\, A_0 = {self._sim.drivingAmplitude},\, \Omega = {self._sim.drivingFreq:.5f},\, \gamma_- = {self._sim.decayConstant}$"
         plt.suptitle(title)
         plt.tight_layout()
+        plt.show()
+        
+    def PlotTotalCurrent(self):
+        """Plots the total current operator in the time and frequency domains."""
+
+        current, fourier = self._sim.CalculateTotalCurrent()
+
+        kValues = (np.array( list( self._sim._models.keys() ) ) / np.pi).tolist()
+        title = rf"$k = {kValues} \pi,\, t_1 = {self._sim.t1},\, t_2 = {self._sim.t2},\, A_0 = {self._sim.drivingAmplitude},\, \Omega = {self._sim.drivingFreq:.5f},\, \gamma_- = {self._sim.decayConstant}$"
+
+        currentLabel = r"$\langle\tilde j_k \rangle$"
+        yLabels = [
+            f"Magnitude of {currentLabel}",
+            f"Real Part of {currentLabel}",
+            f"Imaginary Part of {currentLabel}"
+        ]
+
+        nrows, ncols = 3, 1
+        fig, ax = plt.subplots(nrows, ncols, figsize=(16, 8.8))
+
+        for row in np.arange(nrows):
+            # Plot the numerical solution.
+            ax[row].plot(self._sim.tAxis, self._plottingFunctions[row](current),
+                        color = "Black")
+    
+            ax[row].set_xlabel(self._tLabel)
+            ax[row].set_ylabel(yLabels[row])
+
+        plt.suptitle(title)
+        plt.tight_layout()
+        plt.show()
+
+        plt.figure(figsize=(16, 8.8))
+
+        # Plotting the fourier transform of the current operator.
+        plt.semilogy(self._sim.freqAxis / self._sim.drivingFreq, np.abs(fourier)**2,
+                color = 'black')
+
+        plt.suptitle(title)
+        plt.xlabel(r"$\omega / \Omega$")
+        plt.ylabel(r"$\| \tilde j (\omega) \|^2$")
         plt.show()
