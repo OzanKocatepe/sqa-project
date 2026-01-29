3ee7937d61f79ad10524e0254bd334b631e5af1f
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-01-29 13:46:31 +1100

    For now, moved everything around and broke things up into functions enough, hopefully. Going to try to run the code and fix all the errors. No testing done yet.

--- 1d-ssh-model.py
+++ 1d-ssh-model.py
@@ -2,23 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-from SSH import SSH
-from SSHSimulation import SSHSimulation
-from SSHVisualiser import SSHVisualiser
+from SSHModel import *
 
 tauAxis = np.linspace(0, 300, 200000)
 numT = 1
 initialConditions = np.array([-0.5, -0.5, 0], dtype=complex)
 
-params = {
-    't1' : 2,
-    't2' : 1,
-    'decayConstant' : 0.1,
-    'drivingAmplitude' : 0.2,
-    'drivingFreq' : 2 / 3.01
-}
+params = SSHParameters(
+    t1 = 2,
+    t2 = 1,
+    decayConstant = 0.1,
+    drivingAmplitude = 0.2,
+    drivingFreq = 2 / 3.01
+)
 
-sim = SSHSimulation(**params)
+sim = SSHSimulation(params)
 sim.AddMomentum(np.pi / 4)
 sim.Run(tauAxis, initialConditions, numT, debug=True)
 
--- SSHModel/__init__.py
+++ SSHModel/__init__.py
@@ -1,11 +1,14 @@
 from .CorrelationData import CorrelationData
 from .CurrentData import CurrentData
 from .Fourier import Fourier
+from .SSH import SSH
 from .SSHParameters import SSHParameters
+from .SSHSimulation import SSHSimulation
+from .SSHVisualiser import SSHVisualiser
 
 __all__ = [
-    "CorrelationData",
-    "CurrentData",
-    "Fourier",
-    "SSHParameters"
+    'SSH',
+    'SSHParameters',
+    'SSHSimulation',
+    'SSHVisualiser'
 ]
