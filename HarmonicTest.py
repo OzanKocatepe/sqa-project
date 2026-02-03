0b4e5dfd0a01b88be2f1571f73fd3e5471f6b657
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-02-03 16:39:07 +1100

    Tested the harmonic integration function - the values of the fourier transform at the harmonics seems correct, and so I think the numerical integration is not where the problem lies.

---
+++ HarmonicTest.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from SSHModel import SSH, ModelParameters, CorrelationData, CurrentData
+
+# Defines the irrelevant parameters to be zero.
+params = ModelParameters(
+    k = 0,
+    t1 = 0,
+    t2 = 0,
+    decayConstant = 0,
+    drivingAmplitude = 0,
+    drivingFreq = 2
+)
+
+frequencies = np.array([1, 2, 5]) * params.drivingFreq
+tauAxis = np.linspace(0, 30, 10000)
+signal = np.sum( np.sin( np.outer(2 * np.pi * frequencies, tauAxis) ), axis = 0)
+
+# plt.plot(tauAxis, signal)
+# plt.show()
+
+# Sets up required values.
+ssh = SSH(params)
+ssh._SSH__correlationData = CorrelationData(
+    tauAxisSec = tauAxis
+)
+ssh._SSH__currentData = CurrentData(
+    doubleConnectedCorrelator = signal
+)
+
+# Call function to get harmonics of signal along the entire tauAxis.
+ssh._SSH__ManuallyCalculateFourierAtHarmonics(np.arange(tauAxis.size))
+harmonics = ssh._SSH__currentData.harmonics
+
+plt.semilogy(np.arange(-12, 13), np.abs(harmonics)**2, 'o')
+plt.xlim(-12.5, 12.5)
+plt.show()
