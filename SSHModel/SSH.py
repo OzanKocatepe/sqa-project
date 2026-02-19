Realised the vZ and vPm fourier coefficients were wrong, fixed that and now the solutions only differ by a phase, such that the magnitude has its sign flipped. But both the real and imag parts (for minus and plus, which actually have imaginary parts) are off by some different phase, which doesn't seem like a full or empty phase.

SSHModel/SSH.py
@@ -135,8 +135,8 @@ class SSH:
 
         self.__correlationData.singleTime = self.__CalculateSingleTimeCorrelations(initialConditions, odeParams)
 
-        plusSeries, minusSeries, zSeries = self.__CalculateSingleTimeFourierSeries()
-        self.__correlationData.singleFourierSeries = [plusSeries, minusSeries, zSeries]
+        minusSeries, plusSeries, zSeries = self.__CalculateSingleTimeFourierSeries()
+        self.__correlationData.singleFourierSeries = [minusSeries, plusSeries, zSeries]
 
         self.__correlationData.doubleTime = self.__CalculateDoubleTimeCorrelations(odeParams)
 
@@ -229,16 +229,16 @@ class SSH:
 
         # Solve the system $\mathcal{M}x = b$ by diagonalising $\mathcal M$.
         eigenvalues, U = np.linalg.eig(V)
-        D_inv = np.diag(1 / eigenvalues)
+        D_inv = np.diag(1.0 / eigenvalues)
         U_inv = np.linalg.inv(U)
         sigmaCoeffs = U @ D_inv @ U_inv @ b
 
         # Extracts the coefficients for each individual Fourier series.
-        plusCoeffs = sigmaCoeffs[0:M]
-        minusCoeffs = sigmaCoeffs[M:2*M]
+        minusCoeffs = sigmaCoeffs[0:M]
+        plusCoeffs = sigmaCoeffs[M:2*M]
         zCoeffs = sigmaCoeffs[2*M:]
 
-        return Fourier(self.__params.drivingFreq, plusCoeffs), Fourier(self.__params.drivingFreq, minusCoeffs), Fourier(self.__params.drivingFreq, zCoeffs)
+        return Fourier(self.__params.drivingFreq, minusCoeffs), Fourier(self.__params.drivingFreq, plusCoeffs), Fourier(self.__params.drivingFreq, zCoeffs)
 
     @SSHProfiler.profile 
     def __CalculateVzFourier(self) -> Fourier:
@@ -258,17 +258,15 @@ class SSH:
         vZCoeffs[self.__params.maxN] = self.__params.t2 * np.cos(theta) * (special.jv(0, self.__params.drivingAmplitude) - 1)
 
         for k in range(1, self.__params.maxN + 1):
-            # Useful terms calculated at beginning.
-            vZTerm = self.__params.t2 * special.jv(k, self.__params.drivingAmplitude)
-
             # Even terms.
             if k % 2 == 0:
-                vZCoeffs[self.__params.maxN + k] = vZCoeffs[self.__params.maxN - k] = vZTerm * np.cos(theta) 
+                vZCoeffs[self.__params.maxN + k] = self.__params.t2 * special.jv(k, self.__params.drivingAmplitude) * np.cos(theta)
+                vZCoeffs[self.__params.maxN - k] = self.__params.t2 * special.jv(-k, self.__params.drivingAmplitude) * np.cos(theta)
 
             # Odd terms.
             else:
-                vZCoeffs[self.__params.maxN + k] = -1j * vZTerm * np.sin(theta)
-                vZCoeffs[self.__params.maxN - k] = -vZCoeffs[self.__params.maxN + k]
+                vZCoeffs[self.__params.maxN + k] = -1j * self.__params.t2 * special.jv(k, self.__params.drivingAmplitude) * np.sin(theta)
+                vZCoeffs[self.__params.maxN - k] = 1j * self.__params.t2 * special.jv(-k, self.__params.drivingAmplitude) * np.sin(theta)
 
         return Fourier(self.__params.drivingFreq, vZCoeffs)
 
@@ -290,17 +288,15 @@ class SSH:
         vPmCoeffs[self.__params.maxN] = 1j * self.__params.t2 * np.sin(theta) * (1 - special.jv(0, self.__params.drivingAmplitude))
 
         for k in range(1, self.__params.maxN + 1):
-            # Useful terms calculated at beginning.
-            vPmTerm = 1j * self.__params.t2 * special.jv(k, self.__params.drivingAmplitude)
-
             # Even terms.
             if k % 2 == 0:
-                vPmCoeffs[self.__params.maxN + k] = vPmCoeffs[self.__params.maxN - k] = -vPmTerm * np.sin(theta)
+                vPmCoeffs[self.__params.maxN + k] = -1j * self.__params.t2 * special.jv(k, self.__params.drivingAmplitude) * np.sin(theta)
+                vPmCoeffs[self.__params.maxN - k] = -1j * self.__params.t2 * special.jv(-k, self.__params.drivingAmplitude) * np.sin(theta)
 
             # Odd terms.
             else:
-                vPmCoeffs[self.__params.maxN + k] = vPmTerm * np.cos(theta)
-                vPmCoeffs[self.__params.maxN - k] = -vPmCoeffs[self.__params.maxN + k]
+                vPmCoeffs[self.__params.maxN + k] = 1j * self.__params.t2 * special.jv(k, self.__params.drivingAmplitude) * np.cos(theta)
+                vPmCoeffs[self.__params.maxN - k] = -1j * self.__params.t2 * special.jv(-k, self.__params.drivingAmplitude) * np.cos(theta)
         
         return Fourier(self.__params.drivingFreq, vPmCoeffs)
 

SSHModel/SSHSimulation.py
@@ -207,8 +207,10 @@ class SSHSimulation:
             The directory to save the file to.     
         """
 
-        with gzip.open(f"{fileDir}/numK: {self.numModels}, numT: {self.__axes.tAxisDim.size}.pkl.gz", 'wb') as file:
+        fileName = f"{fileDir}/numK: {self.numModels}, numT: {self.__axes.tAxisDim.size}.pkl.gz"
+        with gzip.open(fileName, 'wb') as file:
             pickle.dump(self, file)
+        print(f"Saved instance to {fileName}.")
 
     @classmethod
     def Load(cls, filePath: str) -> SSHSimulation:
@@ -228,6 +230,7 @@ class SSHSimulation:
         
         with gzip.open(filePath, 'rb') as file:
             obj = pickle.load(file)
+        print(f"Loading instance from {filePath}.")
         return obj
     
     def ExtractModels(self, kArr: float | list[float] | np.ndarray[float]) -> SSHSimulation:

denis-ssh-model.py
@@ -200,7 +200,7 @@ class One_D_SSH_Model:
         # Used to somehow calculate coefficients of fourier series
         # for sigma operators.
         b = np.zeros(3 * M, dtype=complex)
-        # Sets n = 0 coefficient for sigma_z to -2$\gamma$?
+        # Sets the third fourier series to represent -2$\gamma$.
         b[2 * M + num] -= 2.0 * self.gamma
         eig, U, invU = self.quasi_energy(k)
         # Takes inverse of the diagonal eigenvalue matrix
@@ -315,7 +315,7 @@ class One_D_SSH_Model:
         # Axis along which we are solving.
         time = self.time
         # First dimension is number of points in driving period, the other is number of points
-        # in time axis.
+        # in time axis (tAxis, tauAxis).
         correlation = np.zeros((len(self.time_inf), len(time)), dtype=complex)
         # Gets the values of our single-time expectations (in steady state, since these
         # are calculated using fourier series).