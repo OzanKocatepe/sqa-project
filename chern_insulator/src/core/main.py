The getZ function had the wrong sign, so now the solution makes sense, but we STILL have a y-current.

chern_insulator/src/core/main.py
@@ -11,7 +11,7 @@ def main():
     tauMax = 50
 
     params = EnsembleParameters(
-        delta = 1,
+        delta = 3,
         drivingAmp = 0.2,
         decayConstant = 0.2,
         maxN = 50
@@ -22,15 +22,15 @@ def main():
     print(f"Non-trivial Phase (Delta = 1): C = {Hamiltonian.ChernNumber(1)}")
 
     ensemble = Ensemble(params)
-    # ensemble.SampleBrillouinZone(numK)
-    ensemble.AddMomentum((np.pi / 4, np.pi / 8))
+    ensemble.SampleBrillouinZone(numK)
+    # ensemble.AddMomentum((np.pi / 4, np.pi / 8))
     # ensemble.AddMomentum((np.pi / 4, -np.pi / 8))
     # ensemble.AddMomentum((-np.pi / 4, np.pi / 8))
     # ensemble.AddMomentum((-np.pi / 4, -np.pi / 8))
     ensemble.Run(tauMax)
 
     plot = Plotting(ensemble)
-    plot.PlotSingleTime(np.pi / 4, np.pi / 8, tMax = 20, overplotNumericalSolution=True)
+    # plot.PlotSingleTime(np.pi / 4, np.pi / 8, tMax = 20, overplotNumericalSolution=True)
     # plot.PlotSingleTime(np.pi / 4, -np.pi / 8, tMax = 10, overplotNumericalSolution=True)
     # plot.PlotSingleTime(-np.pi / 4, np.pi / 8, tMax = 10, overplotNumericalSolution=True)
     # plot.PlotSingleTime(-np.pi / 4, -np.pi / 8, tMax = 10, overplotNumericalSolution=True)

chern_insulator/src/hamiltonian/Hamiltonian.py
@@ -16,21 +16,4 @@ class Hamiltonian(Base,
                   InternalMixin,
                   DynamicsMixin,
                   TopologyMixin):
-    """Contains the functions derived from the Chern Model Hamiltonian."""
- 
-    @cached_property
-    def rho(self) -> float:
-        """
-        Returns the value of rho, defined as sqrt(hx^2 + hy^2) where hx and hy are undriven terms.
-        This is a commonly used term in the calculations.
-        This result is cached, since rho has no time dependence.
-
-        Returns
-        -------
-        float:
-            The value of rho at this momentum point.
-        """
-
-        raise DeprecationWarning()
-
-        return np.sqrt(self.hx()**2 + self.hy()**2)
+    """Contains the functions derived from the Chern Model Hamiltonian.""" 

chern_insulator/src/hamiltonian/_base.py
@@ -89,11 +89,11 @@ class Base:
 
         t = np.atleast_1d(t)
 
-        H = self.hx(t)[:, np.newaxis, np.newaxis] * self.sigmax + (self.hy() * self.sigmay)[np.newaxis, :, :] + self.hz(t)[: np.newaxis, np.newaxis] * self.sigmaz
+        H = self.hx(t)[:, np.newaxis, np.newaxis] * self.sigmax \
+            + (self.hy() * self.sigmay)[np.newaxis, :, :] \
+            + self.hz(t)[: np.newaxis, np.newaxis] * self.sigmaz
         
-        if t.size == 1:
-            return H[0, :, :]
-        return H
+        return H.squeeze()
 
     def hx(self, t: float | np.ndarray[float]=0) -> float | np.ndarray[float]:
         """
@@ -214,4 +214,10 @@ class Base:
     
     @cached_property
     def U(self) -> np.ndarray[complex]:
-        return np.column_stack((self.plusEigenvector, self.minusEigenvector))
+        U = np.column_stack((self.plusEigenvector, self.minusEigenvector))
+
+        # Raise an exception if U is not unitary.
+        if not np.allclose(U @ U.conj().T, np.eye(2)) or not np.allclose(U.conj().T @ U , np.eye(2)):
+            raise ValueError("Matrix U is not unitary.")
+        
+        return U

chern_insulator/src/hamiltonian/_fourier.py
@@ -44,8 +44,8 @@ class FourierMixin:
         # Calculates the relevant coefficients in a vectorised manner.
         # Not sure if scipy is actually faster vectorised, but regardless it neatens up the code.
         coeffs[oddMask] = np.sign(n[oddMask]) * 1j * special.jv(np.abs(n[oddMask]), self._params.drivingAmp) * np.cos(self._params.kx)
-        coeffs[zeroMask] = special.jv(0, self._params.drivingAmp) * np.sin(self._params.kx)
         coeffs[evenMask] = special.jv(np.abs(n[evenMask]), self._params.drivingAmp) * np.sin(self._params.kx)
+        coeffs[zeroMask] = special.jv(0, self._params.drivingAmp) * np.sin(self._params.kx)
 
         # Returns the array as a float if it has size 1.
         if coeffs.size == 1:
@@ -143,7 +143,7 @@ class FourierMixin:
         """
 
         return np.moveaxis(np.array([[self.hzn(n), self.hxn(n) - 1j * self.hyn(n)],
-                         [self.hxn(n) + 1j * self.hyn(n), -self.hzn(n)]], dtype=complex),
+                                     [self.hxn(n) + 1j * self.hyn(n), -self.hzn(n)]], dtype=complex),
                          -1, 0)
      
     def Hmn(self, n: int | np.ndarray[int]) -> complex | np.ndarray[complex]:

chern_insulator/src/hamiltonian/_internal.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 class InternalMixin:
     """
@@ -16,7 +17,7 @@ class InternalMixin:
         operator: np.ndarray[complex]
     ) -> np.ndarray[complex]:
         """
-        Gets the operator in its eigenbasis.
+        Gets the operator in the band basis.
 
         Parameters
         ----------
@@ -37,8 +38,8 @@ class InternalMixin:
         if operator.ndim == 2:
             operator = operator[np.newaxis, :, :]
 
-        eigenmatrix = (self.U.conj().T @ operator @ self.U)
-        
+        eigenmatrix = self.U.conj().T @ operator @ self.U
+ 
         # Squeezes eigenmatrix to deal with the case whne the shape is
         # (1, 2, 2), so we will get the matrix back as a (2, 2) matrix.
         # Otherwise, returns an (n, n) matrix.
@@ -126,6 +127,6 @@ class InternalMixin:
 
         # Changes the indexing between a single operator and a stack of operators.
         if bandOperator.ndim == 2:
-            return 0.5 * (bandOperator[0, 0] + bandOperator[1, 1])
+            return 0.5 * (bandOperator[0, 0] - bandOperator[1, 1])
         else:
-            return 0.5 * (bandOperator[:, 0, 0] + bandOperator[:, 1, 1])
+            return 0.5 * (bandOperator[:, 0, 0] - bandOperator[:, 1, 1])