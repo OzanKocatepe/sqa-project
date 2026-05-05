99b6a2c530fb0ad4cf6574538240858e2e0104cf
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-05-05 15:28:56 +1000

    Removed the last dredges of the old Hamiltonian class - the heisenberg equations of motion ODE was just moved into the new Hamiltonian class.

--- chern_insulator/src/operators/Hamiltonian.py
+++ chern_insulator/src/operators/Hamiltonian.py
@@ -357,6 +357,40 @@ class Hamiltonian:
 
         coeffs = self.band_fourier_coefficient(n)
         return BandBasisProjector.z_coeff(coeffs)
+
+    def EquationsOfMotion(self, t: float | np.ndarray[float],
+                          c: np.ndarray[complex]
+                          ) -> np.ndarray[complex]:
+        """
+        Returns the right-hand side of the equations of motion for the system at time t, in seconds.
+
+        Parameters
+        ----------
+        t : float | ndarray[float]
+            The time, in seconds, at which to evaluate the equations of motion.
+            Accepts vectorised inputs.
+        c : ndarray[complex]
+            The state of the system at time t, in the band basis.
+            For single-time correlations, c should be (sigma_-, sigma_+, sigma_z).
+
+        Returns
+        -------
+        ndarray[complex]:
+            The right hand side of the equations of motion. If the input is vectorised, the output
+            will also be vectorised, with the first axis having size 3, corresponding to the different operators
+            in c.
+        """
+
+        Hm, Hp, Hz = self.minus(t), self.plus(t), self.z(t)
+        gamma = self._params.decayConstant
+
+        B = np.array([[-(2j * Hz + 0.5 * gamma), 0, 1j * Hp],
+                      [0, 2j * Hz - 0.5 * gamma, -1j * Hm],
+                      [2j * Hm, -2j * Hp, -gamma]], dtype=complex)
+        
+        inhomPart = np.array([0, 0, -gamma], dtype=complex)
+ 
+        return B @ c + inhomPart[:, np.newaxis]
      
     @cached_property
     def plusEigenvector(self) -> np.ndarray[complex]:
