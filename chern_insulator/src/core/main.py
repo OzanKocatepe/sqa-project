8e5f90fd9ef7653fa615262697baf76feecba2e9
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-05-14 15:13:48 +1000

    Added argument parsing to the code.

--- chern_insulator/src/core/main.py
+++ chern_insulator/src/core/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cProfile
+import argparse
 
 from data import EnsembleParameters
 from core.Ensemble import Ensemble
@@ -8,10 +9,44 @@ from core.Plotting import Plotting
 from Topology import ChernNumber
 from config.paths import PLOTTING_DIR, STYLESHEET, DATA_DIR
 
-def main():
-    # Total number of momentum points to sample.
-    numK = 5
-    numT = 5
+def main() -> None:
+    # Create the parser.
+    parser = argparse.ArgumentParser(
+        description = "Solves for the first- and second- order correlation functions for " \
+        "a two-dimensional Chern insulator."
+    )
+    
+    # Define the arguments.
+    parser.add_argument(
+        "-k",
+        "--numK",
+        help = "The number of momentum points on each axis to sample the Brillouin Zone with.",
+        type = int,
+        required = True
+    )
+    parser.add_argument(
+        "-t",
+        "--numT",
+        help = "The number of initial conditions within one driving period to use for the" \
+        "second-order correlation functions.",
+        type = int,
+        required = True
+    )
+    parser.add_argument(
+        "-c",
+        "--cores",
+        help = "The number of cores to use. The input will be sanitized.",
+        type = int,
+        required = True
+    )
+
+    # Parse the arguments.
+    args = parser.parse_args()
+
+    # Defines our values.
+    numK = args.numK
+    numT = args.numT
+    numProcesses = args.cores
     tauMax = 20
 
     # Check the Chern number.
@@ -33,19 +68,19 @@ def main():
     # ensemble.AddMomentum((np.pi / 4, -np.pi / 8))
     # ensemble.AddMomentum((-np.pi / 4, np.pi / 8))
     # ensemble.AddMomentum((-np.pi / 4, -np.pi / 8))
-    ensemble.Run(tauMax, numT, numProcesses=None)
+    ensemble.Run(tauMax, numT, numProcesses=numProcesses)
     # np.save(DATA_DIR / f"D={params.delta}, k={numK}", ensemble.summedCurrent.doubleTimeCurrent)
 
-    plot = Plotting(ensemble)
+    # plot = Plotting(ensemble)
     # plot.PlotSingleTime(np.pi / 4, np.pi / 8, tMax = 20, overplotNumericalSolution=True)
     # plot.PlotSingleTime(np.pi / 4, -np.pi / 8, tMax = 10, overplotNumericalSolution=True)
     # plot.PlotSingleTime(-np.pi / 4, np.pi / 8, tMax = 10, overplotNumericalSolution=True)
     # plot.PlotSingleTime(-np.pi / 4, -np.pi / 8, tMax = 10, overplotNumericalSolution=True)
     # plot.PlotDoubleTimeCorrelation(np.pi / 4, np.pi / 8)
     # plot.PlotDoubleTimeCurrent()
-    plot.PlotTotalCurrent(overplotLengthGauge=False)
+    # plot.PlotTotalCurrent(overplotLengthGauge=False)
     # plot.PlotTotalCurrentFFT(linearScale=False, overplotLengthGauge=False)
 
 if __name__ == "__main__":
-    # cProfile.run(main())
+    # Runs the main function.
     main()
