a79995778222c9f68e1337df58bb844bca4e9b62
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-05-28 16:09:07 +1000

    Improved the legend and colours of the plots and used the stylesheet.

--- chern_insulator/src/plots.py
+++ chern_insulator/src/plots.py
@@ -1,17 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from config.paths import DATA_DIR, PLOTTING_DIR
+from matplotlib.lines import Line2D
+from config.paths import DATA_DIR, PLOTTING_DIR, STYLESHEET
 
 axes, one_current = np.load(DATA_DIR / "D=1.0, k=31.npy", allow_pickle = True)
 _, three_current = np.load(DATA_DIR / "D=3.0, k=31.npy", allow_pickle = True)
 maxN = 50
 
+plt.style.use(STYLESHEET)
+
 # g2(0) FULL SIZE
-plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[0], marker = 'x', label=r"$\Delta = 1$, x")
-plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[1], marker = 'x', label=r"$\Delta = 1$, y")
-plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[0], marker = 'x', label=r"$\Delta = 3$, x")
-plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[1], marker = 'x', label=r"$\Delta = 3$, y")
-plt.legend()
+plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[0], marker = 'x', color='red', linestyle='-')
+plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[1], marker = 'x', color='red', linestyle='--')
+plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[0], marker = 'x', color='blue', linestyle='-')
+plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[1], marker = 'x', color='blue', linestyle='--')
+
+color_handles = [
+    Line2D([0], [0], color='blue', linestyle='-', label='triv'),
+    Line2D([0], [0], color='red',  linestyle='-', label='top'),
+]
+
+style_handles = [
+    Line2D([0], [0], color='k', linestyle='-',  label='x'),
+    Line2D([0], [0], color='k', linestyle='--', label='y'),
+]
+
+legend1 = plt.legend(handles=color_handles, loc='upper left')
+plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+legend2 = plt.legend(handles=style_handles, loc='upper right')
+plt.gca().add_artist(legend2)
+
 plt.xlim((0, 11))
 plt.xlabel(r"$\omega / \Omega$")
 plt.ylabel(r"$g_2(0)$")
@@ -20,14 +38,29 @@ plt.savefig(PLOTTING_DIR / "g2(0).png", dpi=300)
 plt.show()
 
 # MODE NUMBER
-plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[0], marker = 'x', label=r"$\Delta = 1$, x")
-plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[1], marker = 'x', label=r"$\Delta = 1$, y")
-plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[0], marker = 'x', label=r"$\Delta = 3$, x")
-plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[1], marker = 'x', label=r"$\Delta = 3$, y")
-plt.legend()
+plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[0], marker = 'x', color='red', linestyle='-')
+plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[1], marker = 'x', color='red', linestyle='--')
+plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[0], marker = 'x', color='blue', linestyle='-')
+plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[1], marker = 'x', color='blue', linestyle='--')
+
+color_handles = [
+    Line2D([0], [0], color='blue', linestyle='-', label='triv'),
+    Line2D([0], [0], color='red',  linestyle='-', label='top'),
+]
+
+style_handles = [
+    Line2D([0], [0], color='k', linestyle='-',  label='x'),
+    Line2D([0], [0], color='k', linestyle='--', label='y'),
+]
+
+legend1 = plt.legend(handles=color_handles, loc='upper left')
+plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+legend2 = plt.legend(handles=style_handles, loc='upper right')
+plt.gca().add_artist(legend2)
+
 plt.xlim((0, 11))
 plt.xlabel(r"$\omega / \Omega$")
-plt.ylabel(r"$g_2(0)$")
+plt.ylabel(r"$n_{cl}$")
 plt.yscale('log')
 plt.savefig(PLOTTING_DIR / "n_cl.png", dpi=300)
 plt.show()
