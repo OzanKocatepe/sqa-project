Calculated the population variance at weak laser power - also the real part of the time averaged generalised noise correlation tensor. Seems vaguely correct as of now.

chern_insulator/src/core/current_solver.py
@@ -323,7 +323,7 @@ def calculate_spectral_noise_tensor(
     integrand = doubleTimeCurrent[:, :, np.newaxis, :, :] * exponentials[np.newaxis, np.newaxis, :, np.newaxis, :]
 
     gamma_m = np.arange(1, n + 1)**2 * params.decayConstant
-    amps = 1 / (2 * gamma_m * omega_m)
+    amps = params.matter_light_coupling**2 / (2 * gamma_m * omega_m)
 
     return amps[np.newaxis, np.newaxis, :, np.newaxis] * np.trapezoid(
         y = integrand,
@@ -381,13 +381,63 @@ def calculate_dc_population_variance(
     integrand = averaged_current[:, :, np.newaxis, :] * exponentials[np.newaxis, np.newaxis, :, :]
 
     gamma_m = np.arange(1, n + 1)**2 * params.decayConstant
-    amps = 1 / (2 * gamma_m * omega_m)
+    amps = params.matter_light_coupling**2 / (2 * gamma_m * omega_m)
 
     return amps[np.newaxis, np.newaxis, :] * np.trapezoid(
         y = integrand,
         x = tauAxis,
         axis = 3
     )
+
+def calculate_dc_population_variance_weak_laser_power(
+        params: ModelParameters,
+        scattering_rate: float | np.ndarray[float]
+) -> np.ndarray[complex]:
+    """Calculates the dc population variance at some weak laser power.
+
+    Note that this must be calculated at each momentum individually, since
+    the summand is non-linear in the momentum.
+
+    Parameters
+    ----------
+    params : ModeLparameters
+        The parameters of the model.
+    scattering_rate : float | ndarray[float]
+        The scattering rate of the system. Can be given as a float or a vector
+        of scattering rates. Note that this is the value of gamma_M, NOT the
+        parameterised value of gamma_M / Delta.
+
+    Returns
+    -------
+    ndarray[complex]
+        The dc population variance at weak laser power for m = 1, ..., maxN. This is also the real part of the
+        generalised noise correlation tensor, averaged over the t-axis. Has shape (2, m), where the
+        first axis corresponds to direction and m corresponds to mode. If scattering_rate is given as a vector,
+        then the shape is (2, m, scattering_rate.size).
+    """
+
+    scattering_rate = np.atleast_1d(scattering_rate)[np.newaxis, np.newaxis, :]
+
+    # Calculates the x- and y- paramagnetic current operators in the band basis.
+    band_basis = hamiltonian.get_band_basis(params)
+    x_current_operator = band_basis_projector.rotate_to_band_basis(band_basis, ParamagneticCurrentX.lattice_basis(params, 0))
+    y_current_operator = band_basis_projector.rotate_to_band_basis(band_basis, ParamagneticCurrentX.lattice_basis(params, 0))
+
+    # Calculates the j^{-+}j^{+-} term. Since these are just scalars they commute, so doesn't really matter
+    # which is which - they are just the off-diagonal elements.
+    off_diagonal_current = np.array([
+        x_current_operator[0, 1] * x_current_operator[1, 0],
+        y_current_operator[0, 1] * y_current_operator[1, 0]
+    ])[:, np.newaxis, np.newaxis]
+
+    gamma_m = params.decayConstant * (np.arange(1, params.maxN + 1)**2)[np.newaxis, :, np.newaxis]
+    omega_m = params.angularFreq * np.arange(1, params.maxN + 1)[np.newaxis, :, np.newaxis]
+
+    # Should have shape (mu, m, gamma).
+    return (params.matter_light_coupling**2 / (2 * gamma_m * omega_m)) * (
+        (2 * scattering_rate * off_diagonal_current) 
+        / (scattering_rate**2 + (2 * hamiltonian.energy(params) + omega_m)**2)
+    ).squeeze()
 
 def calculate_current_fourier_coefficients(
     params: ModelParameters,
@@ -461,7 +511,7 @@ def calculate_semiclassical_mode_population(
     omega_m = params.angularFreq * np.arange(1, m + 1)[np.newaxis, :, np.newaxis]
     omega_p = params.angularFreq * np.arange(-m, m + 1)[np.newaxis, np.newaxis, :]
 
-    mode_population = (1 / omega_m) * np.abs(jp)**2 / (
+    mode_population = (params.matter_light_coupling**2 / omega_m) * np.abs(jp)**2 / (
         gamma_m**2 + (omega_m - omega_p)**2
     )
     # Sums over p. Final shape is (mu, m).

chern_insulator/src/data/Parameters.py
@@ -30,12 +30,14 @@ class EnsembleParameters:
     drivingAmp: float
     decayConstant: float
     maxN: int
+    num_particles: int
 
     # Not set to init=False, since we want to explicitly set it
     # when we create a ModelParameters instance, rather than recalculating it
     # in __post_init__.
     drivingFreq: float = field(default=None)
     angularFreq: float = field(init = False)
+    matter_light_coupling: float = field(default = None)
 
     def __post_init__(self) -> None:
         """
@@ -56,6 +58,9 @@ class EnsembleParameters:
 
             self.angularFreq = bandGap * 2 / 5
             self.drivingFreq =  self.angularFreq / (2 * np.pi)
+
+        # Set g_0 using beta_0 = 1.
+        self.matter_light_coupling = self.num_particles**(-3/4)
 
 @dataclass(slots=True)
 class ModelParameters(EnsembleParameters):
@@ -105,7 +110,9 @@ class ModelParameters(EnsembleParameters):
             drivingAmp = params.drivingAmp,
             drivingFreq = params.drivingFreq,
             decayConstant = params.decayConstant,
-            maxN = params.maxN
+            maxN = params.maxN,
+            num_particles = params.num_particles,
+            matter_light_coupling = params.matter_light_coupling
         )
 
         # Must set outside constructor, since angular freq is not in the constructor.

chern_insulator/src/plots.py
@@ -10,60 +10,60 @@ maxN = 50
 plt.style.use(STYLESHEET)
 
 # g2(0) FULL SIZE
-plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[0], marker = 'x', color='red', linestyle='-')
-plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[1], marker = 'x', color='red', linestyle='--')
-plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[0], marker = 'x', color='blue', linestyle='-')
-plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[1], marker = 'x', color='blue', linestyle='--')
+# plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[0], marker = 'x', color='red', linestyle='-')
+# plt.plot(np.arange(1, maxN + 1), one_current.second_order_correlation_function[1], marker = 'x', color='red', linestyle='--')
+# plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[0], marker = 'x', color='blue', linestyle='-')
+# plt.plot(np.arange(1, maxN + 1), three_current.second_order_correlation_function[1], marker = 'x', color='blue', linestyle='--')
 
-color_handles = [
-    Line2D([0], [0], color='blue', linestyle='-', label='triv'),
-    Line2D([0], [0], color='red',  linestyle='-', label='top'),
-]
+# color_handles = [
+#     Line2D([0], [0], color='blue', linestyle='-', label='triv'),
+#     Line2D([0], [0], color='red',  linestyle='-', label='top'),
+# ]
 
-style_handles = [
-    Line2D([0], [0], color='k', linestyle='-',  label='x'),
-    Line2D([0], [0], color='k', linestyle='--', label='y'),
-]
+# style_handles = [
+#     Line2D([0], [0], color='k', linestyle='-',  label='x'),
+#     Line2D([0], [0], color='k', linestyle='--', label='y'),
+# ]
 
-legend1 = plt.legend(handles=color_handles, loc='upper left')
-plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
-legend2 = plt.legend(handles=style_handles, loc='upper right')
-plt.gca().add_artist(legend2)
+# legend1 = plt.legend(handles=color_handles, loc='upper left')
+# plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+# legend2 = plt.legend(handles=style_handles, loc='upper right')
+# plt.gca().add_artist(legend2)
 
-plt.xlim((0, 9))
-plt.xlabel(r"$\omega / \Omega$")
-plt.ylabel(r"$g_2(0)$")
+# plt.xlim((0, 9))
+# plt.xlabel(r"$\omega / \Omega$")
+# plt.ylabel(r"$g_2(0)$")
+# # plt.yscale('log')
+# plt.savefig(PLOTTING_DIR / "g2(0).png", dpi=300)
+# plt.show()
+
+# # MODE NUMBER
+# plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[0], marker = 'x', color='red', linestyle='-')
+# plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[1], marker = 'x', color='red', linestyle='--')
+# plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[0], marker = 'x', color='blue', linestyle='-')
+# plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[1], marker = 'x', color='blue', linestyle='--')
+
+# color_handles = [
+#     Line2D([0], [0], color='blue', linestyle='-', label='triv'),
+#     Line2D([0], [0], color='red',  linestyle='-', label='top'),
+# ]
+
+# style_handles = [
+#     Line2D([0], [0], color='k', linestyle='-',  label='x'),
+#     Line2D([0], [0], color='k', linestyle='--', label='y'),
+# ]
+
+# legend1 = plt.legend(handles=color_handles, loc='upper left')
+# plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+# legend2 = plt.legend(handles=style_handles, loc='upper right')
+# plt.gca().add_artist(legend2)
+
+# plt.xlim((0, 9))
+# plt.xlabel(r"$\omega / \Omega$")
+# plt.ylabel(r"$n_{cl}$")
 # plt.yscale('log')
-plt.savefig(PLOTTING_DIR / "g2(0).png", dpi=300)
-plt.show()
-
-# MODE NUMBER
-plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[0], marker = 'x', color='red', linestyle='-')
-plt.plot(np.arange(1, 3 * maxN + 1), one_current.semiclassical_mode_population[1], marker = 'x', color='red', linestyle='--')
-plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[0], marker = 'x', color='blue', linestyle='-')
-plt.plot(np.arange(1, 3 * maxN + 1), three_current.semiclassical_mode_population[1], marker = 'x', color='blue', linestyle='--')
-
-color_handles = [
-    Line2D([0], [0], color='blue', linestyle='-', label='triv'),
-    Line2D([0], [0], color='red',  linestyle='-', label='top'),
-]
-
-style_handles = [
-    Line2D([0], [0], color='k', linestyle='-',  label='x'),
-    Line2D([0], [0], color='k', linestyle='--', label='y'),
-]
-
-legend1 = plt.legend(handles=color_handles, loc='upper left')
-plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
-legend2 = plt.legend(handles=style_handles, loc='upper right')
-plt.gca().add_artist(legend2)
-
-plt.xlim((0, 9))
-plt.xlabel(r"$\omega / \Omega$")
-plt.ylabel(r"$n_{cl}$")
-plt.yscale('log')
-plt.savefig(PLOTTING_DIR / "n_cl.png", dpi=300)
-plt.show()
+# plt.savefig(PLOTTING_DIR / "n_cl.png", dpi=300)
+# plt.show()
 
 # g2(0) ZOOMED IN
 # plt.plot(np.arange(1, 51), one_current.second_order_correlation_function[0], marker = 'x', label=r"$\Delta = 1$, x")
@@ -77,3 +77,32 @@ plt.show()
 # plt.ylabel(r"$g_2(0)$")
 # plt.savefig(PLOTTING_DIR / "[Zoomed] g2(0).png", dpi=300)
 # plt.show()
+
+# delta n
+for gamma_m_index in range(one_current.dc_population_variance_weak_laser.shape[2]):
+    plt.plot(np.arange(1, maxN + 1), one_current.dc_population_variance_weak_laser[0, :, gamma_m_index], marker = 'x', color='red', linestyle='-')
+    plt.plot(np.arange(1, maxN + 1), one_current.dc_population_variance_weak_laser[1, :, gamma_m_index], marker = 'x', color='red', linestyle='--')
+    plt.plot(np.arange(1, maxN + 1), three_current.dc_population_variance_weak_laser[0, :, gamma_m_index], marker = 'x', color='blue', linestyle='-')
+    plt.plot(np.arange(1, maxN + 1), three_current.dc_population_variance_weak_laser[1, :, gamma_m_index], marker = 'x', color='blue', linestyle='--')
+
+color_handles = [
+    Line2D([0], [0], color='blue', linestyle='-', label='triv'),
+    Line2D([0], [0], color='red',  linestyle='-', label='top'),
+]
+
+style_handles = [
+    Line2D([0], [0], color='k', linestyle='-',  label='x'),
+    Line2D([0], [0], color='k', linestyle='--', label='y'),
+]
+
+legend1 = plt.legend(handles=color_handles, loc='upper left')
+plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+legend2 = plt.legend(handles=style_handles, loc='upper right')
+plt.gca().add_artist(legend2)
+
+plt.xlim((0, 9))
+plt.xlabel(r"$\omega / \Omega$")
+plt.ylabel(r"$\delta n_{\mu, m}$")
+# plt.yscale('log')
+plt.savefig(PLOTTING_DIR / "n_cl.png", dpi=300)
+plt.show()