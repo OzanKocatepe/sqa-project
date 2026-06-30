e21c5e19fc46285ae2458a38c991dc334054460f
Ozan Kocatepe <ozan@Ozans-MacBook-Pro.local>
2026-06-30 12:23:54 +1000

    Plotted the angular momentum on a symmetric log plot.

--- chern_insulator/src/plots.py
+++ chern_insulator/src/plots.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+from matplotlib.ticker import SymmetricalLogLocator
 from config.paths import DATA_DIR, PLOTTING_DIR, STYLESHEET
 
 axes, one_bz_average_current, one_ensemble_current = np.load(DATA_DIR / "A=0.2, D=1.0, k=21.npy", allow_pickle = True)
@@ -109,59 +110,59 @@ plt.style.use(STYLESHEET)
 
 # squeezing
 # Determine the number of gamma indices to calculate the color gradients
-num_gamma = one_ensemble_current.squeezing_weak_laser.shape[2]
+# num_gamma = one_ensemble_current.squeezing_weak_laser.shape[2]
 
-# Sample from the Reds and Blues colormaps. 
-# We start at 0.4 to ensure the lightest colors remain visible against a white background.
-red_shades = plt.cm.Reds(np.linspace(0.4, 1.0, num_gamma))
-blue_shades = plt.cm.Blues(np.linspace(0.4, 1.0, num_gamma))
+# # Sample from the Reds and Blues colormaps. 
+# # We start at 0.4 to ensure the lightest colors remain visible against a white background.
+# red_shades = plt.cm.Reds(np.linspace(0.4, 1.0, num_gamma))
+# blue_shades = plt.cm.Blues(np.linspace(0.4, 1.0, num_gamma))
 
-# Plot the lines using the generated shades
-for gamma_m_index in range(num_gamma):
-    current_red = red_shades[gamma_m_index]
-    current_blue = blue_shades[gamma_m_index]
+# # Plot the lines using the generated shades
+# for gamma_m_index in range(num_gamma):
+#     current_red = red_shades[gamma_m_index]
+#     current_blue = blue_shades[gamma_m_index]
     
-    plt.plot(np.arange(1, maxN + 1), one_ensemble_current.squeezing_weak_laser[0, :, gamma_m_index], marker='x', color=current_red, linestyle='-')
-    plt.plot(np.arange(1, maxN + 1), one_ensemble_current.squeezing_weak_laser[1, :, gamma_m_index], marker='x', color=current_red, linestyle='--')
-    plt.plot(np.arange(1, maxN + 1), three_ensemble_current.squeezing_weak_laser[0, :, gamma_m_index], marker='x', color=current_blue, linestyle='-')
-    plt.plot(np.arange(1, maxN + 1), three_ensemble_current.squeezing_weak_laser[1, :, gamma_m_index], marker='x', color=current_blue, linestyle='--')
+#     plt.plot(np.arange(1, maxN + 1), one_ensemble_current.squeezing_weak_laser[0, :, gamma_m_index], marker='x', color=current_red, linestyle='-')
+#     plt.plot(np.arange(1, maxN + 1), one_ensemble_current.squeezing_weak_laser[1, :, gamma_m_index], marker='x', color=current_red, linestyle='--')
+#     plt.plot(np.arange(1, maxN + 1), three_ensemble_current.squeezing_weak_laser[0, :, gamma_m_index], marker='x', color=current_blue, linestyle='-')
+#     plt.plot(np.arange(1, maxN + 1), three_ensemble_current.squeezing_weak_laser[1, :, gamma_m_index], marker='x', color=current_blue, linestyle='--')
 
-# Dynamically build the color legend handles to include the gamma_m_index
-gamma_m = np.log10(np.logspace(-4, -1, 5))
-color_handles = []
-for i in range(num_gamma):
-    color_handles.append(Line2D([0], [0], color=blue_shades[i], linestyle='-', label=rf'$\gamma_m / \Delta = 10^{{{gamma_m[i]:.2f}}}$'))
-for i in range(num_gamma):
-    color_handles.append(Line2D([0], [0], color=red_shades[i], linestyle='-', label=rf'$\gamma_m / \Delta = 10^{{{gamma_m[i]:.2f}}}$'))
+# # Dynamically build the color legend handles to include the gamma_m_index
+# gamma_m = np.log10(np.logspace(-4, -1, 5))
+# color_handles = []
+# for i in range(num_gamma):
+#     color_handles.append(Line2D([0], [0], color=blue_shades[i], linestyle='-', label=rf'$\gamma_m / \Delta = 10^{{{gamma_m[i]:.2f}}}$'))
+# for i in range(num_gamma):
+#     color_handles.append(Line2D([0], [0], color=red_shades[i], linestyle='-', label=rf'$\gamma_m / \Delta = 10^{{{gamma_m[i]:.2f}}}$'))
 
-style_handles = [
-    Line2D([0], [0], color='k', linestyle='-',  label='x'),
-    Line2D([0], [0], color='k', linestyle='--', label='y'),
-]
+# style_handles = [
+#     Line2D([0], [0], color='k', linestyle='-',  label='x'),
+#     Line2D([0], [0], color='k', linestyle='--', label='y'),
+# ]
 
-top_handles = [
-    Line2D([0], [0], color='blue', linestyle='-',  label='triv'),
-    Line2D([0], [0], color='red', linestyle='-', label='top'),
-]
+# top_handles = [
+#     Line2D([0], [0], color='blue', linestyle='-',  label='triv'),
+#     Line2D([0], [0], color='red', linestyle='-', label='top'),
+# ]
 
-# Configure legends
-legend1 = plt.legend(handles=color_handles, loc='upper right', fontsize='small', ncol=2) 
-plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
+# # Configure legends
+# legend1 = plt.legend(handles=color_handles, loc='upper right', fontsize='small', ncol=2) 
+# plt.gca().add_artist(legend1)  # needed so the first legend isn't overwritten
 
-legend2 = plt.legend(handles=style_handles, loc='upper right', bbox_to_anchor=(1.0, 0.825))
-plt.gca().add_artist(legend2)
+# legend2 = plt.legend(handles=style_handles, loc='upper right', bbox_to_anchor=(1.0, 0.825))
+# plt.gca().add_artist(legend2)
 
-legend3 = plt.legend(handles=top_handles, loc='upper right', bbox_to_anchor=(0.94, 0.825))
-plt.gca().add_artist(legend3)
+# legend3 = plt.legend(handles=top_handles, loc='upper right', bbox_to_anchor=(0.94, 0.825))
+# plt.gca().add_artist(legend3)
 
-plt.xlim((0, 9))
-plt.xlabel(r"$\omega / \Omega$")
-plt.ylabel(r"$\eta_{\mu, m}$")
-# plt.axhline(0, color='black')
-# plt.yscale('log')
+# plt.xlim((0, 9))
+# plt.xlabel(r"$\omega / \Omega$")
+# plt.ylabel(r"$\eta_{\mu, m}$")
+# # plt.axhline(0, color='black')
+# # plt.yscale('log')
 
-plt.savefig(PLOTTING_DIR / "squeezing_weak_laser.png", dpi=300)
-plt.show()
+# plt.savefig(PLOTTING_DIR / "squeezing_weak_laser.png", dpi=300)
+# plt.show()
 
 # Plot the lines using the generated shades 
 # plt.plot(np.arange(1, maxN + 1), one_current.squeezing[0, :], marker='x', color="red", linestyle='-')
@@ -195,22 +196,51 @@ plt.show()
 # plt.savefig(PLOTTING_DIR / "squeezing.png", dpi=300)
 # plt.show()
 
-plt.plot(np.arange(1, maxN + 1), np.abs(one_ensemble_current.angular_momentum), marker='x', color="red", linestyle='-')
-plt.plot(np.arange(1, maxN + 1), np.abs(three_ensemble_current.angular_momentum), marker='x', color="blue", linestyle='-')
+plt.rcParams.update({'font.size' : 12})
+plt.plot(np.arange(1, maxN + 1), one_ensemble_current.angular_momentum,   marker='x', color="red",  linestyle='-', markersize=10)
+plt.plot(np.arange(1, maxN + 1), three_ensemble_current.angular_momentum, marker='x', color="blue", linestyle='-', markersize=10)
 
 top_handles = [
     Line2D([0], [0], color='blue', linestyle='-',  label='triv'),
     Line2D([0], [0], color='red', linestyle='-', label='top'),
 ]
 
-legend3 = plt.legend(handles=top_handles, loc='upper right', bbox_to_anchor=(0.94, 0.825))
+legend3 = plt.legend(handles=top_handles, loc='upper right')
 plt.gca().add_artist(legend3)
 
-plt.xlim((0, 9))
+plt.xlim((1, 9))
+plt.ylim((-1e-2, 1e-4))
 plt.xlabel(r"$\omega / \Omega$")
 plt.ylabel(r"$L_m$")
 # plt.axhline(0, color='black')
-plt.yscale('log')
+# plt.yscale('symlog', linthresh=1e-11, linscale=0.5)
+# plt.yscale('asinh')
+# plt.minorticks_on()
+# plt.grid(which="both", axis='y')
+
+ax = plt.gca()
+
+ax.set_yscale(
+    'symlog',
+    linthresh=1e-9,
+    linscale=0.5
+)
+
+# ax.yaxis.set_minor_locator(
+#     SymmetricalLogLocator(
+#         base=10,
+#         linthresh=1e-11,
+#         subs=np.arange(2,10)
+#     )
+# )
+
+# ax.grid(which='major', axis='y')
+# ax.grid(which='minor', axis='y', alpha=0.3)
 
 plt.savefig(PLOTTING_DIR / "angular_momentum.png", dpi=300)
 plt.show()
+
+# plt.plot(np.arange(1, maxN + 1), np.abs(one_ensemble_current.angular_momentum / three_ensemble_current.angular_momentum))
+# plt.xlim((1, 9))
+# plt.ylabel(r"$L_{top} / L_{triv}$")
+# plt.show()
