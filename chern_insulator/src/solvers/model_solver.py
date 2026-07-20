Generated some new plots. Fixed a scaling problem in the second order correlation function so now it has the correct scale (~1). Also, changed the matter correlation tensor to be D(t + tau, t) rather than D(t, t + tau). This only comes into affect in the second order connected current (i.e. D) in the code. Everything before is still calculated using the other convention. This fixed the squeezing issue, it now matches up with the weak-laser approximation.

chern_insulator/src/solvers/model_solver.py
@@ -219,7 +219,7 @@ def calculate_double_time_current(
         An array of shape (2, 2, tAxis.size, tauAxis.size) containing the double-time current correlations.
         The first and second axes correspond to the direction of the current operator, with indices
         0 and 1 corresponding to the x- and y- current respectively.
-        The last two axes correspond to the double-time correlator j_alpha(t) j_beta(t + tau) at times
+        The last two axes correspond to the double-time correlator j_alpha(t + tau) j_beta(t) at times
         t and t + tau.
     """
 
@@ -269,7 +269,7 @@ def calculate_double_time_current(
                         * connected
                     )
 
-    return doubleCurrentCorrelations 
+    return np.swapaxes(doubleCurrentCorrelations, 0, 1).conj()
 
 def imaginary_time_avg_generalised_noise_correlation_tensor_weak_laser(
     params : ModelParameters,